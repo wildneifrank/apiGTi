c0f479e40f6158483be358b78794e2893dc20730
wildneifrank <wildneif2@gmail.com>
2023-05-24 16:17:01 -0300

    correção

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import fastify from "fastify";
 import { dataRoutes } from "./routes/data";
 
-const app = fastify()
+const app = fastify();
 
-app.register(dataRoutes)
+app.register(dataRoutes);
 
-app.listen({
-  port: 5050
-}).then(() =>{
-  console.log('Servidor iniciado')
-})
+app.listen({ port: 5050 || 3000, host: "0.0.0.0" }).then(() => {
+  console.log("Servidor iniciado");
+});
