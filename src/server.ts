3e84259354180d61adbc7a9c66d10a0158a175e5
wildneifrank <wildneif2@gmail.com>
2023-05-24 16:30:44 -0300

    tasa

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
 import { dataRoutes } from "./routes/data";
+import dotenv from "dotenv";
 
+dotenv.config();
+const port = process.env.PORT;
 const app = fastify();
 
 app.register(dataRoutes);
 
-app.listen({ port: 5050 || 3000, host: "0.0.0.0" }).then(() => {
+app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" }).then(() => {
   console.log("Servidor iniciado");
 });
